b5f4aaa6fec5822b0e79a6f6b35d3bcf10df9a5f
Adi Mihaila <adrian.mihaila@emag.ro>
2020-07-29 17:09:27 +0300

    first commit

---
+++ src/ConsoleAvailabilityBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace EmagTechLabs\ConsoleAvailabilityBundle;
+
+use EmagTechLabs\ConsoleAvailabilityBundle\DependencyInjection\Compiler\CompilerPass;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Class ConsoleAvailabilityBundle
+ * @package EmagTechLabs\ConsoleAvailabilityBundle
+ */
+class ConsoleAvailabilityBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new CompilerPass($container->environment));
+    }
+}
